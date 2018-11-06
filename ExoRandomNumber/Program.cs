1bdcedadfd3cb66692deaff25e967a231384eb6f
nbl241 <39767922+nbl241@users.noreply.github.com>
2018-11-06 15:07:55 +0100

    ExoRandomNumber

--- ExoRandomNumber/Program.cs
+++ ExoRandomNumber/Program.cs
@@ -10,6 +10,48 @@ namespace ExoRandomNumber
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int rnd = random.Next(1, 100);
+
+            bool win = false;
+            while (win == false)
+            {
+                Console.WriteLine("indiquez un chiffre entre 1 et 100");
+                string chiffre = Console.ReadLine();
+                bool success = int.TryParse(chiffre, out int rep);
+                Console.WriteLine();
+
+                if (success && rep < 101 && rep > 0)
+                {
+                    if (rep > rnd)
+                    {
+                        Console.WriteLine("Nombre plus grand");
+                    }
+
+                    else if (rep < rnd)
+                    {
+                        Console.WriteLine("Nombre plus petit");
+                    }
+                    else
+                    {
+                        win = true;
+                    }
+                        Console.WriteLine();
+                }
+
+                else
+                {
+                    Console.WriteLine("erreur de saisie");
+                    Console.WriteLine();
+                }
+            }
+
+            if (win)
+            {
+                Console.WriteLine("Bingo !!");
+            }
+
+            Console.ReadLine();
         }
     }
 }
