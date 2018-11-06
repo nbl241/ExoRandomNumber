a6901c49fb49ce408d1a15f6cec55a3347625f1c
nbl241 <39767922+nbl241@users.noreply.github.com>
2018-11-06 17:05:05 +0100

    Ajout de la sauvegarde sur un fichier

--- ExoRandomNumber/Program.cs
+++ ExoRandomNumber/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,38 @@ namespace ExoRandomNumber
 {
     class Program
     {
+        static string path = @"c:\temp\myTest.txt";
+        static string name;
+
         static void Main(string[] args)
+        {
+            bool isPlay = true;
+            while (isPlay)
+            {
+                Play();
+                {
+                    Console.WriteLine("Rejouer ? O / N");
+                    string rep = Console.ReadLine();
+                    if (rep.ToUpper() != "O")
+                    {
+                        isPlay = false;
+                    }
+                }
+            }
+        }
+
+        public static void Play()
         {
             Random random = new Random();
             int rnd = random.Next(1, 100);
 
-            bool win = false;
-            while (win == false)
+            List<int> AllReponse = new List<int>();
+
+            bool hasWin = false;
+            while (hasWin == false)
             {
                 Console.WriteLine("indiquez un chiffre entre 1 et 100");
+
                 string chiffre = Console.ReadLine();
                 bool success = int.TryParse(chiffre, out int rep);
                 Console.WriteLine();
@@ -25,18 +49,19 @@ namespace ExoRandomNumber
                 {
                     if (rep > rnd)
                     {
-                        Console.WriteLine("Nombre plus grand");
+                        Console.WriteLine("C'est moins");
                     }
 
                     else if (rep < rnd)
                     {
-                        Console.WriteLine("Nombre plus petit");
+                        Console.WriteLine("C'est plus");
                     }
                     else
                     {
-                        win = true;
+                        hasWin = true;
                     }
-                        Console.WriteLine();
+                    Console.WriteLine();
+                    AllReponse.Add(rnd);
                 }
 
                 else
@@ -46,12 +71,29 @@ namespace ExoRandomNumber
                 }
             }
 
-            if (win)
+            if (hasWin)
             {
                 Console.WriteLine("Bingo !!");
             }
+            else
+            {
+                Console.WriteLine("Désolé vous avez perdu. La réponse était {0}", rnd);
+            }
+            Console.WriteLine();
+            AllReponse = new List<int>();
 
-            Console.ReadLine();
+            string path = @"c:\temp\myTest.txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Résultat pour {0}", name);
+                    sw.WriteLine("--------------------------------------");
+                    sw.WriteLine("La réponse était: {0}", rnd);
+                    //sw.WriteLine("Voici vos réponse: {0}", );
+                }
+            }
         }
     }
 }
+
