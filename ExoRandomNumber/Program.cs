6721549ed22a67c467f30bd5fdd0c88813cb4453
nabil bellil <abilaz24@gmail.com>
2018-11-07 02:11:28 +0100

    Ajout sauvegarde des score dans testScore.txt

--- ExoRandomNumber/Program.cs
+++ ExoRandomNumber/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,12 +10,18 @@ namespace ExoRandomNumber
 {
     class Program
     {
-        static string path = @"c:\temp\myTest.txt";
+        static string result = @"c:\temp\testResult.txt";
+        static string score = @"c:\temp\testScore.txt";
         static string name;
+
 
         static void Main(string[] args)
         {
             bool isPlay = true;
+
+            Console.WriteLine("Entrez votre nom");
+            name = Console.ReadLine();
+
             while (isPlay)
             {
                 Play();
@@ -34,12 +41,14 @@ namespace ExoRandomNumber
             Random random = new Random();
             int rnd = random.Next(1, 100);
 
+            int cpt = 0;
+            bool hasWin = false;
+
             List<int> AllReponse = new List<int>();
 
-            bool hasWin = false;
             while (hasWin == false)
             {
-                Console.WriteLine("indiquez un chiffre entre 1 et 100");
+                Console.WriteLine("indiquez un chiffre entre 1 et 100 (Essai {0})", cpt);
 
                 string chiffre = Console.ReadLine();
                 bool success = int.TryParse(chiffre, out int rep);
@@ -60,8 +69,9 @@ namespace ExoRandomNumber
                     {
                         hasWin = true;
                     }
+                    cpt++;
                     Console.WriteLine();
-                    AllReponse.Add(rnd);
+                    AllReponse.Add(rep);
                 }
 
                 else
@@ -80,17 +90,66 @@ namespace ExoRandomNumber
                 Console.WriteLine("Désolé vous avez perdu. La réponse était {0}", rnd);
             }
             Console.WriteLine();
+            FileResult(rnd, cpt, AllReponse, name);
+            ScoreResult(name, cpt);
             AllReponse = new List<int>();
+        }
 
-            string path = @"c:\temp\myTest.txt";
-            if (!File.Exists(path))
+        public static void ScoreResult(string name, int cpt)
+        {
+            int id = 0;
+            id++;
+
+            if (!File.Exists(score))
             {
-                using (StreamWriter sw = File.CreateText(path))
+                using (StreamWriter sw = File.CreateText(score))
                 {
-                    sw.WriteLine("Résultat pour {0}", name);
+                    sw.WriteLine("Classement général {0}");
+                    sw.WriteLine("**************************************");
+                    sw.WriteLine();
+                    sw.WriteLine("Class;Nom;Point");
                     sw.WriteLine("--------------------------------------");
-                    sw.WriteLine("La réponse était: {0}", rnd);
-                    //sw.WriteLine("Voici vos réponse: {0}", );
+                    sw.WriteLine("{0}. {1}: {2}", id, name, cpt);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(score))
+                {
+                    sw.WriteLine("{0}. {1}: {2}", id, name, cpt);
+                }
+            }
+        }
+
+        public static void FileResult(int rnd, int cpt, List<int> AllResponse, string name)
+        {
+            if (!File.Exists(result))
+            {
+                using (StreamWriter sw = File.CreateText(result))
+                {
+                    sw.WriteLine("Résultat pour {0}, le {1}", name, DateTime.Now.ToShortDateString());
+                    sw.WriteLine("--------------------------------------");
+                    sw.WriteLine("La bonne réponse est: {0}", rnd);
+                    sw.WriteLine("Nombre d'essais: {0}", cpt);
+                    sw.WriteLine("Voici vos réponse: ");
+                    AllResponse.ForEach(x => sw.Write(x.ToString() + " - "));
+                    sw.WriteLine();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(result))
+                {
+                    sw.WriteLine();
+                    sw.WriteLine("***************************************");
+                    sw.WriteLine();
+                    sw.WriteLine("Résultat pour {0}, le {1}", name, DateTime.Now.ToShortDateString());
+                    sw.WriteLine("--------------------------------------");
+                    sw.WriteLine("La bonne réponse est: {0}", rnd);
+                    sw.WriteLine("Nombre d'essais: {0}", cpt);
+                    sw.WriteLine("Voici vos réponse: ");
+                    AllResponse.ForEach(x => sw.Write(x.ToString() + " - "));
+                    sw.WriteLine();
                 }
             }
         }
